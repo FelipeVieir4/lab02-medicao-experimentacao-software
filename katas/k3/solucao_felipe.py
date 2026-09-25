335d732f795599a047415b4f29b7a00fb5736279
Felipe <luizfelipevieira01@gmail.com>
2026-09-24 21:18:38 -0300

    trial: if you can read this manual (#21)
    Implementa o k3 manual do Felipe e arquiva a solução final validada com 3/3 testes passando.

--- katas/k3/solucao_felipe.py
+++ katas/k3/solucao_felipe.py
@@ -9,4 +9,32 @@
 
 
 def to_nato(words):
-	pass
+	alphabet = [
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+		'U', 'V', 'W', 'X', 'Y', 'Z'
+	]
+
+	nato = [
+		'Alfa', 'Bravo', 'Charlie', 'Delta', 'Echo', 'Foxtrot',
+		'Golf', 'Hotel', 'India', 'Juliett', 'Kilo', 'Lima',
+		'Mike', 'November', 'Oscar', 'Papa', 'Quebec', 'Romeo',
+		'Sierra', 'Tango', 'Uniform', 'Victor', 'Whiskey',
+		'Xray', 'Yankee', 'Zulu'
+	]
+
+	result = []
+
+	for char in words:
+		if char == ' ':
+			continue
+
+		char = char.upper()
+
+		if char in alphabet:
+			position = alphabet.index(char)
+			result.append(nato[position])
+		else:
+			result.append(char)
+
+	return " ".join(result)
