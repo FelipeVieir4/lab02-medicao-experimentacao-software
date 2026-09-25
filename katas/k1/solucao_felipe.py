e66976651e9bb85cd12499814b9eb228e3741328
Felipe <luizfelipevieira01@gmail.com>
2026-09-24 21:18:28 -0300

    trial: connect four manual (#19)
    Implementa o k1 manual do Felipe, arquiva a solução final e registra o trial validado com 2/2 testes passando.

--- katas/k1/solucao_felipe.py
+++ katas/k1/solucao_felipe.py
@@ -13,4 +13,30 @@
 
 
 def connect_four_place(columns):
-	pass
+	tab = []
+
+	for i in range(6):
+		row = []
+		for j in range(7):
+			row.append('-')
+		tab.append(row)
+
+	is_yellow = True
+
+	for col in columns:
+		if is_yellow:
+			token = 'Y'
+		else:
+			token = 'R'
+
+		for row in range(5, -1, -1):
+			if tab[row][col] == '-':
+				tab[row][col] = token
+				break
+
+		if is_yellow:
+			is_yellow = False
+		else:
+			is_yellow = True
+
+	return tab
